31cd3ff731ac16036b08e56aaf8b5c20b41d8e9b
Daniel <daniel.unverricht@pentlandfirth.com>
2020-01-28 08:46:09 +0100

    fix license

--- src/cmd/dummy.go
+++ src/cmd/dummy.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2020 NAME HERE <EMAIL ADDRESS>
+Copyright © 2020 Daniel Unverricht <daniel@unvericht.net>
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
